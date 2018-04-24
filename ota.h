a5ecbfd21f4b8a9a0ceea755b770888201b3f479
Jared Quinn <jared@pydiem.com>
2018-04-24 19:26:28 +1000

    Update OTA Functions to use new UI

--- ota.h
+++ ota.h
@@ -1,35 +1,21 @@
 void ota_setup() {
   ArduinoOTA
   .onStart([]() {
-    runState = 255;
-    
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-//    ledcWrite(pwm_ledChannel, 255);
-    myscreen.fillScreen(ILI9341_BLACK);
-    myscreen.setTextSize(2);
-    myscreen.setCursor(0, 0);
-    myscreen.println("OTA Update: " + type);
-
-    Serial.println("Start updating " + type);
+    ui.updateStatus("Rcv. OTA Update", ui.screen->color565(255,0,0), false);
   })
   .onEnd([]() {
-    Serial.println("\nEnd");
+    ui.updateStatus("Finishing Update", ui.screen->color565(0,255,0), false);
   })
   .onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+//    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   })
   .onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+//    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) ui.updateStatus("OTA: Auth Failed", ui.screen->color565(255,0,0), false);
+    else if (error == OTA_BEGIN_ERROR) ui.updateStatus("OTA: Begin Failed", ui.screen->color565(255,0,0), false);
+    else if (error == OTA_CONNECT_ERROR) ui.updateStatus("OTA: Connect Failed", ui.screen->color565(255,0,0), false);
+    else if (error == OTA_RECEIVE_ERROR) ui.updateStatus("OTA: Rcv Error", ui.screen->color565(255,0,0), false);
+    else if (error == OTA_END_ERROR) ui.updateStatus("OTA: End Failed", ui.screen->color565(255,0,0), false);
   });
 
   ArduinoOTA.begin();
